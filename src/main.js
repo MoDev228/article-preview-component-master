64f16975019e905686d95bbfaf498813c9820112
MoDev228 <mb98402074@gmail.com>
2026-06-01 14:22:46 +0000

    Fin du projet et deployement

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import "./styles/main.css";
+
+const shareBtn = document.querySelector(".share-btn");
+const shareBubble = document.querySelector(".share-bubble");
+
+shareBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  shareBubble.classList.toggle("active");
+});
